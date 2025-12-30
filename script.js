f35b11d9103f286055c9959eea2dfb7465b0714b
Ekrem Tezcan Sarıdağ <ets123main@gmail.com>
2025-12-30 14:56:08 +0300

    v2 🚀
    🔤 Kelime üretme sistemi eklendi
    🎲 Kelimeler rastgele konumlandırıldı
    ⬇️ Belirli aralıklarla aşağı doğru hareket etmeleri sağlandı

--- script.js
+++ script.js
@@ -1,15 +1,94 @@
-var words = ["ev","elma" ,"evet", "araba", "masa", "kitap", "kalem", "defter", "bilgisayar", "telefon", "bardak", "sandalye", "kapı"]
+var words = [
+    "ev", "elma", "evet", "araba", "masa", "kitap", "kalem", "defter", "bilgisayar", "telefon",
+    "bardak", "sandalye", "kapı", "pencere", "oda", "mutfak", "banyo", "yatak", "koltuk", "sehpa",
+    "halı", "perde", "ayna", "saat", "lamba", "çanta", "cüzdan", "anahtar", "gözlük", "şapka",
+    "elbise", "pantolon", "gömlek", "tişört", "ayakkabı", "çorap", "kemer", "atkı", "eldiven", "şemsiye",
+    "tabak", "çatal", "kaşık", "bıçak", "tencere", "tava", "fırın", "buzdolabı", "çamaşır", "bulaşık",
+    "sabun", "şampuan", "diş", "fırça", "macun", "havlu", "tuvalet", "kağıt", "çöp", "kova",
+    "süpürge", "faraş", "deterjan", "bez", "kova", "pas", "paspas", "ütü", "ütü", "masası",
+    "çamaşır", "makinesi", "bulaşık", "makinesi", "kurutma", "makinesi", "elektrik", "süpürgesi", "mikrodalga", "fırın",
+    "tost", "makinesi", "çay", "makinesi", "kahve", "makinesi", "blender", "robot", "süpürge", "drone", "kamera",
+    "hoparlör", "kulaklık", "fare", "klavye", "monitör", "yazıcı", "tarayıcı", "projektör", "modem", "router",
+    "harddisk", "usb", "bellek", "hafıza", "kartı", "pil", "şarj", "cihazı", "adaptör", "kablo",
+    "uzatma", "kablosu", "priz", "anahtar", "sigorta", "ampul", "led", "ışık", "fener", "mum",
+    "kibrit", "çakmak", "gazete", "dergi", "roman", "hikaye", "şiir", "mektup", "zarf", "pul",
+    "kartpostal", "fotoğraf", "resim", "tablo", "heykel", "vazo", "çiçek", "bitki", "saksı", "toprak",
+    "su", "hava", "güneş", "ay", "yıldız", "bulut", "yağmur", "kar", "dolu", "rüzgar",
+    "şimşek", "gökkuşağı", "deniz", "göl", "nehir", "dağ", "orman", "ağaç", "yaprak", "çimen",
+    "taş", "kaya", "kum", "toprak", "çamur", "yol", "cadde", "sokak", "meydan", "park",
+    "bahçe", "tarla", "çiftlik", "köy", "şehir", "kasaba", "ülke", "kıta", "dünya", "evren",
+    "insan", "kadın", "erkek", "çocuk", "bebek", "aile", "anne", "baba", "kardeş", "abi",
+    "abla", "dede", "nine", "torun", "akraba", "arkadaş", "komşu", "öğrenci", "öğretmen", "doktor",
+    "hemşire", "mühendis", "mimar", "işçi", "memur", "sanatçı", "sporcu", "şarkıcı", "oyuncu", "yazar",
+    "gazeteci", "polis", "asker", "itfaiyeci", "pilot", "kaptan", "aşçı", "garson", "berber", "kuaför",
+    "terzi", "ayakkabıcı", "tamirci", "temizlikçi", "bahçıvan", "çoban", "balıkçı", "avcı", "çiftçi", "madenci",
+    "bankacı", "muhasebeci", "avukat", "hakim", "savcı", "noter", "eczacı", "veteriner", "dişçi", "psikolog",
+    "sosyolog", "filozof", "tarihçi", "coğrafyacı", "matematikçi", "fizikçi", "kimyager", "biyolog", "astronom", "arkeolog",
+    "antropolog", "ekonomist", "siyasetçi", "diplomat", "bakan", "başkan", "kral", "kraliçe", "prens", "prenses",
+    "imparator", "imparatoriçe", "sultan", "halife", "papa", "rahip", "imam", "hoca", "şeyh", "derviş",
+    "budist", "hindu", "yahudi", "hristiyan", "müslüman", "ateist", "deist", "agnostik", "spiritüalist", "mistik",
+    "yoga", "meditasyon", "namaz", "oruç", "hac", "zekat", "sadaka", "dua", "ibadet", "tapınak",
+    "cami", "kilise", "sinagog", "manastır", "türbe", "mezarlık", "kabir", "anıt", "heykel", "müze",
+    "galeri", "kütüphane", "okul", "üniversite", "hastane", "klinik", "eczane", "banka", "postane", "otel",
+    "restoran", "kafe", "bar", "disko", "sinema", "tiyatro", "konser", "stadyum", "spor", "salonu",
+    "havuz", "plaj", "piknik", "alanı", "kamp", "alanı", "orman", "parkı", "hayvanat", "bahçesi",
+    "akvaryum", "lunapark", "alışveriş", "merkezi", "çarşı", "pazar", "market", "bakkal", "kasap", "manav",
+    "fırın", "pastane", "kuruyemişçi", "aktar", "eczane", "optik", "saatçi", "kuyumcu", "terzi", "kuru",
+    "temizlemeci", "ayakkabıcı", "tamirci", "oto", "servis", "benzin", "istasyonu", "otogar", "havalimanı", "liman",
+    "tren", "garı", "metro", "istasyonu", "tramvay", "durağı", "otobüs", "durağı", "taksi", "durağı",
+    "vapur", "iskelesi", "köprü", "tünel", "viyadük", "baraj", "gölet", "kanal", "kuyu", "çeşme",
+    "şelale", "volkan", "deprem", "tsunami", "sel", "yangın", "fırtına", "kasırga", "hortum", "çığ",
+    "heyelan", "erozyon", "kuraklık", "salgın", "hastalık", "kaza", "felaket", "savaş", "barış", "özgürlük",
+    "adalet", "eşitlik", "demokrasi", "cumhuriyet", "monarşi", "diktatörlük", "sosyalizm", "kapitalizm", "komünizm", "faşizm",
+    "milliyetçilik", "ırkçılık", "feminizm", "çevrecilik", "veganizm", "vejetaryenlik", "sağlık", "hastalık", "ilaç", "tedavi",
+    "ameliyat", "aşı", "vitamin", "mineral", "protein", "karbonhidrat", "yağ", "şeker", "tuz", "su",
+    "yemek", "içecek", "kahvaltı", "öğle", "yemeği", "akşam", "yemeği", "tatlı", "meyve", "sebze",
+    "et", "tavuk", "balık", "süt", "peynir", "yoğurt", "ekmek", "pilav", "makarna", "çorba",
+    "salata", "zeytin", "bal", "reçel", "tereyağı", "yumurta", "baharat", "sos", "turşu", "cips",
+    "çikolata", "şekerleme", "dondurma", "pasta", "kurabiye", "kek", "bisküvi", "gofret", "kraker", "simit",
+    "poğaça", "börek", "pide", "lahmacun", "döner", "köfte", "kebap", "pizza", "hamburger", "sandviç"
+];
 
-function rastgele(min, max) {
-    var rastgelesayi = Math.floor(Math.random() * (max-min) + min);
-    
-    console.log(rastgelesayi);
-    return rastgelesayi;
+var aktifKelime = [];
+
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-rastgele(5, 10);
 
 
-// words dizisinden rastgele kelime seç 
-function KelimeYarat(){
+function generateRandomWord() {
+    var randomSayi = getRandomNumber(0,words.length);
+
+    var secilenkelime = words[randomSayi];
+
+    const yeniDiv = document.createElement("div");
+
+    yeniDiv.innerText = secilenkelime;
+    yeniDiv.classList.add("kelime");// css'de stil vermek için bunu ekliyoruz 
+
+    yeniDiv.style.left = getRandomNumber(0, 500) + "px";
+
+    yeniDiv.style.top = "0px"
+
+    var gameBoard = document.getElementById("game-board");
+    gameBoard.appendChild(yeniDiv);
+    aktifKelime.push(yeniDiv);
+
 
 }
+
+setInterval(generateRandomWord, 3000);
+
+function hareket(){
+    aktifKelime.forEach(function(yeniDiv){
+        var asilYükseklik = parseInt(yeniDiv.style.top) || 0;
+
+        asilYükseklik = asilYükseklik + 2;
+
+        yeniDiv.style.top = asilYükseklik + "px";
+    })
+}
+
+setInterval(hareket,30);
+
+generateRandomWord();
